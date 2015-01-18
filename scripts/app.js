858fad211b3679f02716591c554ab2bb887bd282
Nate <nate@lybstore.com>
2015-01-18 01:02:09 +0000

    Create app.js

---
+++ scripts/app.js
@@ -0,0 +1,22 @@
+'use strict';
+	//defines all the dependencies of the application in general
+	 angular
+		.module("uoApp", ['ui.router','translationModule', 'itemsModule'])
+		 .config(['$urlRouterProvider', '$stateProvider','translationConfProvider', function($urlRouterProvider, $stateProvider, translationConfProvider) {
+			 $urlRouterProvider.otherwise('/');
+			 $stateProvider
+				 .state('home' , {
+					 url: '/',
+					 templateUrl: 'app/templates/home.html',
+					 controller:  'itemCtrl as ctrl'
+				 });
+
+			 translationConfProvider.defaultLang('en');
+			 translationConfProvider.translations(
+				 {
+					 'en' : {'recommendation' : 'If you like this, you might be into these'},
+					 'es': {'recommendation': 'Si te gusto esto, puede interesarte'}
+
+				 })
+
+		 }]);
