256c61c0feb6917a36b8b033f4cde014360df12d
Nate <nate@lybstore.com>
2015-01-19 01:15:40 +0000

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,9 @@
-'use strict';
+	
+	'use strict';
 	//defines all the dependencies of the application in general
 	 angular
 		.module("uoApp", ['ui.router','translationModule', 'itemsModule'])
-		.constant('currencies', {'en': "USD", 'es': "€"})
+		 .constant('currencies', {'en': "£", 'es': "€"})
 		 .config(['$urlRouterProvider', '$stateProvider','translationConfProvider', function($urlRouterProvider, $stateProvider, translationConfProvider) {
 			 $urlRouterProvider.otherwise('/');
 			 $stateProvider
@@ -12,13 +13,12 @@
 					 controller:  'itemCtrl as ctrl'
 				 });
 
-			 translationConfProvider.defaultLang('es');
+			 translationConfProvider.defaultLang('en');
 			 translationConfProvider.translations(
 				 {
 					 'en' : {'recommendation' : 'If you like this, you might be into these'},
 					 'es': {'recommendation': 'Si te gusto esto, puede interesarte'}
 
 				 })
-
 
 		 }]);
